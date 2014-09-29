dc0a54e4bceb9fe529fda54b8cf42ae590896801
Garming <garming_sam@outlook.com>
2014-09-29 21:51:54 +1300

    Added p179, 'sieve' is inefficient

---
+++ Project Euler/problem179.py
@@ -0,0 +1,73 @@
+import collections
+
+def primes2(n):
+    """ Input n>=6, Returns a list of primes, 2 <= p < n """
+    n, correction = n-n%6+6, 2-(n%6>1)
+    sieve = [True] * (n/3)
+    for i in xrange(1,int(n**0.5)/3+1):
+      if sieve[i]:
+        k=3*i+1|1
+        sieve[      k*k/3      ::2*k] = [False] * ((n/6-k*k/6-1)/k+1)
+        sieve[k*(k-2*(i&1)+4)/3::2*k] = [False] * ((n/6-k*(k-2*(i&1)+4)/6-1)/k+1)
+    return [2,3] + [3*i+1|1 for i in xrange(1,n/3-correction) if sieve[i]]
+
+val = None
+def primes(n):
+    global val
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    val = [[] for x in xrange(n)]
+    for i in xrange(3,n,1):
+        for x in xrange(i, n, i):
+            sieve[x] = False
+            val[x].append(i)
+    return [2] + [i for i in xrange(3,n,2) if sieve[i]]
+
+primes(10 ** 7)
+print "DONE"
+this = True
+last = -1
+lastlist = []
+count = 0
+for x in xrange(2, 10**7):
+    somelist = val[x][:]
+    if this:
+        somelist.append(2)
+    somelist.append(1)
+    if len(somelist) == last:
+        count += 1
+        #print somelist, lastlist, len(somelist), len(lastlist)
+    last = len(somelist)
+    lastlist = somelist
+    #print somelist
+    this = not this
+    
+
+"""a = primes2(int((10**7)** 0.5)+1)
+b = set(a)
+
+c = [-1 for x in xrange(10 ** 7)]
+print "HELLO"
+for x in xrange(2, 10**7):
+    # prime factorise
+    primes = []
+    val = x
+    i = 0
+    found = False
+    while a[i] < val ** 0.5 + 1:
+        if val % a[i] == 0:
+            val /= a[i]
+            primes.append(a[i])
+            if c[val] != -1:
+                primes += c[val]
+                val = 1
+        elif val == 1:
+            found = True
+        elif val in b:
+            primes.append(val)
+            val = 1
+        else:
+            i += 1
+    if not found and val != 1:
+        primes.append(val)
+    c[x] = primes"""
