b7daa367751a230f4d49c3cb6fc755afbdbe1abe
GSam <1924133+GSam@users.noreply.github.com>
2025-02-25 16:42:50 +1300

    Update Elf.java
    This problem took me all the time to solve. It's worth noting what the problem was as the solution does not seem clear from it.

--- 2014 Code Jam/Round 1C/Elf.java
+++ 2014 Code Jam/Round 1C/Elf.java
@@ -1,3 +1,12 @@
+/*
+* Vida says she's part Elf: that at least one of her ancestors was an Elf. But she doesn't know if it was a parent (1 generation ago), a grandparent (2 generations ago), or someone from even more generations ago. Help her out!
+*
+* Being part Elf works the way you probably expect. People who are Elves, Humans and part-Elves are all created in the same way: two parents get together and have a baby. If one parent is A/B Elf, and the other parent is C/D Elf, then their baby will be (A/B + C/D) / 2 Elf. For example, if someone who is 0/1 Elf and someone who is 1/2 Elf have a baby, that baby will be 1/4 Elf.
+*
+* Vida is certain about one thing: 40 generations ago, she had 240 different ancestors, and each one of them was 1/1 Elf or 0/1 Elf.
+* 
+* Vida says she's P/Q Elf. Tell her what is the minimum number of generations ago that there could have been a 1/1 Elf in her family. If it is not possible for her to be P/Q Elf, tell her that she must be wrong!
+*/
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
